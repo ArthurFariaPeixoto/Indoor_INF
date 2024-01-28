20aa9e07f10947d87d401355167b045111d3dd84
arthu <arthurfpeixoto@gmail.com>
2024-01-28 02:17:25 -0300

    Conexão entre nodulos, cor de rota, mapa INF

--- code/MyApplication3/app/src/main/java/com/example/myapplication/Graph.kt
+++ code/MyApplication3/app/src/main/java/com/example/myapplication/Graph.kt
@@ -8,77 +8,6 @@ data class Node(val id: Int, val x: Float, val y: Float){
     }
 }
 
-//class Graph {
-//
-//    private val nodes = mutableListOf<Node>()
-//    private var shortestPath = emptyList<Node>()
-//
-//    fun addNode(id: Int, x: Float, y: Float) {
-//        nodes.add(Node(id, x, y))
-//    }
-//
-//    fun getNodes(): List<Node> {
-//        return nodes
-//    }
-//
-//    fun calculateShortestPath(startNodeId: Int, endNodeId: Int): List<Node> {
-//        val startNode = nodes.find { it.id == startNodeId }
-//        val endNode = nodes.find { it.id == endNodeId }
-//
-//        if (startNode != null && endNode != null) {
-//            val distances = mutableMapOf<Node, Float>()
-//            val previousNodes = mutableMapOf<Node, Node?>()
-//            val unvisitedNodes = nodes.toMutableList()
-//
-//            distances[startNode] = 0f
-//
-//            while (unvisitedNodes.isNotEmpty()) {
-//                val currentNode = unvisitedNodes.minByOrNull { distances.getOrDefault(it, Float.POSITIVE_INFINITY) }!!
-//
-//                unvisitedNodes.remove(currentNode)
-//
-//                for (neighbor in unvisitedNodes) {
-//                    val tentativeDistance = distances.getOrDefault(currentNode, Float.POSITIVE_INFINITY) +
-//                            calculateDistance(currentNode, neighbor)
-//
-//                    if (tentativeDistance < distances.getOrDefault(neighbor, Float.POSITIVE_INFINITY)) {
-//                        distances[neighbor] = tentativeDistance
-//                        previousNodes[neighbor] = currentNode
-//                    }
-//                }
-//            }
-//
-//            shortestPath = buildPath(startNode, endNode, previousNodes)
-//            return shortestPath
-//        }
-//
-//        return emptyList()
-//    }
-//
-//    fun getShortestPath(): List<Node> {
-//        return shortestPath
-//    }
-//
-//    private fun calculateDistance(node1: Node, node2: Node): Float {
-//        val deltaX = node2.x - node1.x
-//        val deltaY = node2.y - node1.y
-//        return Math.sqrt((deltaX * deltaX + deltaY * deltaY).toDouble()).toFloat()
-//    }
-//
-//    private fun buildPath(startNode: Node, endNode: Node, previousNodes: Map<Node, Node?>): List<Node> {
-//        val path = mutableListOf<Node>()
-//        var currentNode: Node? = endNode
-//
-//        while (currentNode != null) {
-//            path.add(currentNode)
-//            currentNode = previousNodes[currentNode]
-//        }
-//
-//        return path.reversed()
-//    }
-//}
-
-
 class Graph {
 
     private val nodes = mutableListOf<Node>()
@@ -144,8 +73,6 @@ class Graph {
 
         return emptyList()
     }
-
-
 
     fun getShortestPath(): List<Node> {
         return shortestPath
